8de175456aaf6a250a1a08e3b1c102ba24a5c8d4
Emma <brantled@mail.uc.edu>
2022-09-13 14:50:31 -0400

    checking tests

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,26 +1,27 @@
 // Elements
 const welcomeScreen = document.getElementById(`welcome-screen`);
-const gameScreen = ;
-const startGameButton = ;
-const userName = ;
-const userSelection = ;
-const goButton = ;
-const scoreParagraph = ;
-const gameHistoryParagraph = ;
+const gameScreen = document.getElementById(`game-screen`);
+const startGameButton = document.getElementById(`start-game-button`);
+const userName = document.getElementById(`username`);
+const userSelection = document.getElementById(`user-selection`);
+const goButton = document.getElementById(`go-button`);
+const scoreParagraph = document.getElementById(`score`);
+const gameHistoryParagraph = document.getElementById(`game-history`);
 
 // instantiate the game object from the `RockPaperScissors` class.
 let game;
 
 // hide game screen
-gameScreen.classList.add(`d-none`);
+welcomeScreen.classList.add(`d-none`);
 
 // updateScoreTallyUI
 function updateScoreTallyUI(){
-
+  scoreParagraph = game.username + ": " + game.score + "v CPU: " + game.score
 }
 
 // updateGameHistoryUI
 function updateGameHistoryUI(){
+gameHistoryParagraph = "";
 
 }
 
