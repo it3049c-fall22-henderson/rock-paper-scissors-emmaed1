033109322c719ec5895dcc6bd10beb2e9075a70f
Emma <brantled@mail.uc.edu>
2022-09-13 13:48:01 -0400

    finished rock paper scissors

--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -15,7 +15,14 @@ class RockPaperScissors {
   generateCPUResponse(){
     const acceptedValues = [ `rock`, `paper`, `scissors` ];
 
-    return ;
+    if(Math.floor(Math.random()*3) == 0){
+      return "rock"
+    }else if(Math.floor(Math.random()*3) == 1){
+      return "paper"
+    }else if(Math.floor(Math.random()*3) == 2){
+      return "scissors"
+    }
+
   }
   /**
    * returns one of the following values: `win`, `lose`, `tie`
@@ -33,7 +40,17 @@ class RockPaperScissors {
    * @param {string} cpuSelection computer selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   determineWinner(userSelection, cpuSelection){
-
+    if (userSelection == cpuSelection){
+      return "tie"
+    }else if (userSelection == "rock" && cpuSelection == "scissors"){
+      return "win"
+    }else if (userSelection == "paper" && cpuSelection == "rock"){
+      return "win"
+    }else if (userSelection == "scissors" && cpuSelection == "paper"){
+      return "win"
+    }else{
+      return "lose"
+    }
   }
 
   /**
@@ -41,7 +58,13 @@ class RockPaperScissors {
    * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
    */
   play(userSelection){
-
+    generateCPUResponse()
+    determineWinner(userSelection, cpuSelection)
+      if("win"){
+        this.score.user ++;
+      }else if("lose"){
+        this.score.cpu ++;
+      }
   }
 
 }
